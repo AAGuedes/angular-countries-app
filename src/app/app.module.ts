754603fdda3e5dc0ac7b26367dd7e07636a320f6
AAGuedes <airam.a.g.85@gmail.com>
2023-04-22 16:30:58 +0100

    Countries Service - Restcountries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +14,7 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     AppRoutingModule,
     BrowserModule,
+    HttpClientModule,
     SharedModule,
   ],
   providers: [],
