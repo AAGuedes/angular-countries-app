25c87207e2cf3d75dfb23c0a183510f9bb0c60a0
AAGuedes <airam.a.g.85@gmail.com>
2023-04-23 19:23:48 +0100

    Mostrar y ocultar el LoadingSpinner

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -11,15 +11,18 @@ import { Country } from '../../interfaces/country';
 export class ByCapitalPageComponent {
 
   public countries: Country[] = [];
+  public isLoading: boolean = false;
 
   constructor(
     private contriesService: CountriesService
   ) {}
 
   searchByCapital(term: string): void {
+    this.isLoading = true;
     this.contriesService.searchCapital(term)
       .subscribe(resp => {
         this.countries = resp;
+        this.isLoading = false;
     })
   }
 
